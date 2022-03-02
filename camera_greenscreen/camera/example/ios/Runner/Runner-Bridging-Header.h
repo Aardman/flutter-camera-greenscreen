4522cebeb7bc076189709e16832efb97a6486131
Paul Freeman <paul@paulfreeman.com>
2022-03-02 12:11:27 +0000

    Add Swift bridging header

---
+++ camera_greenscreen/camera/example/ios/Runner/Runner-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+//Can import the integration point, if desired to enable Swift code to replace the initial plugin registry
+//And any subsequent Objc classes required by Swift.
+
+//Makes this avaiable to Swift code
+//#import "GeneratedPluginRegistrant.h"
