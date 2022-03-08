5fa85181a374f3d55e8822419e353cf50777030d
Paul Freeman <paul@paulfreeman.com>
2022-03-08 16:47:58 +0000

    Delete unused extension for CGImage

--- camera_greenscreen/camera/ios/Classes/FilterPipeline.swift
+++ camera_greenscreen/camera/ios/Classes/FilterPipeline.swift
@@ -120,18 +120,7 @@ public class FilterPipeline : NSObject {
        
 }
 
-
-
-///Helper extension  converts CGImage to Data
-extension CGImage {
-    var png: Data? {
-        guard let mutableData = CFDataCreateMutable(nil, 0),
-            let destination = CGImageDestinationCreateWithData(mutableData, "public.png" as CFString, 1, nil) else { return nil }
-        CGImageDestinationAddImage(destination, self, nil)
-        guard CGImageDestinationFinalize(destination) else { return nil }
-        return mutableData as Data
-    }
-}
+ 
 
 
 
