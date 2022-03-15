ae22f8712cca3652462a6f1b19a126a41617adc3
Paul Freeman <paul@paulfreeman.com>
2022-03-15 15:36:16 +0000

    Added stub classes and refs to GPUImage built test app with dependencies

---
+++ camera_greenscreen/camera/android/src/main/java/io/flutter/plugins/camera/FilterParameters.java
@@ -0,0 +1,11 @@
+package io.flutter.plugins.camera;
+
+/**
+ *  FilterParameters is a DAO for the updatable parameters for the Filters
+ */
+
+public final class FilterParameters {
+
+    public FilterParameters() {}
+
+}
