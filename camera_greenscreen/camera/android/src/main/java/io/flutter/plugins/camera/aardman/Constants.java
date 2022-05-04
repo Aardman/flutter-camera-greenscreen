a8abead51e16d6f40e68421430220e68c6a46343
Paul Freeman <paul@paulfreeman.com>
2022-05-04 11:53:40 +0100

    Squashed commit of the following:
    commit 174dcaac113ac9ab0415e4118ad0da563237e6f0
    Author: Paul Freeman <paul@paulfreeman.com>
    Date:   Wed May 4 11:53:15 2022 +0100
    
        Bump version to +39
    
    commit bd70f7797e8532d8058bd115d8ebccca24fa8664
    Author: Paul Freeman <paul@paulfreeman.com>
    Date:   Wed May 4 11:53:03 2022 +0100
    
        Fixed setting default parameters when not all params are provided

---
+++ camera_greenscreen/camera/android/src/main/java/io/flutter/plugins/camera/aardman/Constants.java
@@ -0,0 +1,9 @@
+package io.flutter.plugins.camera.aardman;
+
+/**
+ * Used by FilterRenderer for updating float values from parameters
+ */
+public class Constants {
+    public static final float FLOAT_NOT_SET = -1.0f;
+    public static final float [] DEFAULT_COLOUR =  {0.0f, 1.0f, 0.0f};
+}
